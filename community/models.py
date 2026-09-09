7fc0fd038a8cd75e5f83ec7c7a0a67d4cfc0fdec
starearthrocket <aliceharperoverton@yahoo.com>
2026-09-09 13:46:05 +0100

    Implement CommunityPost model

--- community/models.py
+++ community/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class CommunityPost(models.Model):
+    """A community post created by an I-V Tree user."""
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name='community_posts',
+        null=True,
+        blank=True,
+    )
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    image = models.ImageField(
+        upload_to='community-posts/',
+        blank=True,
+    )
+    is_published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
--- community/tests.py
+++ community/tests.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from .models import CommunityPost
+
+
+class CommunityPostModelTest(TestCase):
+    def test_community_post_creation(self):
+        user = User.objects.create_user(
+            username='testuser',
+            password='testpassword123'
+        )
+
+        post = CommunityPost.objects.create(
+            author=user,
+            title='Helping an ivy-covered tree',
+            content='A short community update about protecting a tree.'
+        )
+
+        self.assertEqual(post.author, user)
+        self.assertTrue(post.is_published)
+        self.assertEqual(
+            str(post),
+            'Helping an ivy-covered tree'
+        )
