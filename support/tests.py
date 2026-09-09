0ed4821a027880119b46529378dd2db78161f398
starearthrocket <aliceharperoverton@yahoo.com>
2026-09-09 13:48:37 +0100

    Add failing Donation model test

--- support/tests.py
+++ support/tests.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Donation
+
+
+class DonationModelTest(TestCase):
+    def test_donation_defaults(self):
+        user = User.objects.create_user(
+            username='testuser',
+            password='testpassword123'
+        )
+
+        donation = Donation.objects.create(
+            user=user,
+            amount='10.00',
+            stripe_session_id='cs_test_example'
+        )
+
+        self.assertEqual(donation.user, user)
+        self.assertEqual(donation.payment_status, 'PENDING')
+        self.assertEqual(str(donation), '£10.00 donation')
