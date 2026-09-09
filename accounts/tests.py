91668324af48d3969cf349aa2839b01941e78523
starearthrocket <aliceharperoverton@yahoo.com>
2026-09-09 13:40:50 +0100

    Add failing Profile model test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Profile
+
+
+class ProfileModelTest(TestCase):
+    def test_profile_links_to_user(self):
+        user = User.objects.create_user(
+            username='testuser',
+            password='testpassword123'
+        )
+
+        profile = Profile.objects.create(
+            user=user,
+            avatar_choice='avatar-oak-leaf.png'
+        )
+
+        self.assertEqual(profile.user, user)
+        self.assertEqual(
+            profile.avatar_choice,
+            'avatar-oak-leaf.png'
+        )
