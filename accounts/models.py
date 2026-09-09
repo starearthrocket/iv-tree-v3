e92190a729a6ef19e47df704ab066bd0cef32b97
starearthrocket <aliceharperoverton@yahoo.com>
2026-09-09 13:42:40 +0100

    Implement user Profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,38 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    """Additional profile information for an I-V Tree user."""
+
+    class Avatar(models.TextChoices):
+        OAK_LEAF = 'avatar-oak-leaf.png', 'Oak Leaf'
+        ACORN = 'avatar-acorn.png', 'Acorn'
+        ROBIN = 'avatar-robin.png', 'Robin'
+        FOX = 'avatar-fox.png', 'Fox'
+        HEDGEHOG = 'avatar-hedgehog.png', 'Hedgehog'
+        FERN = 'avatar-fern.png', 'Fern'
+        BADGER = 'avatar-badger.png', 'Badger'
+        SQUIRREL = 'avatar-squirrel.png', 'Squirrel'
+        DEER = 'avatar-deer.png', 'Deer'
+        PINECONE = 'avatar-pinecone.png', 'Pinecone'
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='profile',
+    )
+    avatar_choice = models.CharField(
+        max_length=40,
+        choices=Avatar.choices,
+        default=Avatar.OAK_LEAF,
+    )
+    bio = models.TextField(
+        max_length=500,
+        blank=True,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user.username} profile'
