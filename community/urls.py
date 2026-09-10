47d38137004ea996878895879a70bc993e00c705
starearthrocket <aliceharperoverton@yahoo.com>
2026-09-10 13:49:55 +0100

    Complete community post CRUD

--- community/urls.py
+++ community/urls.py
@@ -15,4 +15,19 @@ urlpatterns = [
         views.post_create,
         name='post_create',
     ),
+    path(
+        '<int:pk>/',
+        views.post_detail,
+        name='post_detail',
+    ),
+    path(
+        '<int:pk>/edit/',
+        views.post_edit,
+        name='post_edit',
+    ),
+    path(
+        '<int:pk>/delete/',
+        views.post_delete,
+        name='post_delete',
+    ),
 ]
--- community/views.py
+++ community/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import CommunityPostForm
 from .models import CommunityPost
@@ -16,6 +17,25 @@ def post_list(request):
         'community/post_list.html',
         {
             'posts': posts,
+        },
+    )
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(
+        CommunityPost.objects.select_related('author'),
+        pk=pk,
+    )
+
+    if not post.is_published:
+        if request.user != post.author:
+            raise Http404
+
+    return render(
+        request,
+        'community/post_detail.html',
+        {
+            'post': post,
         },
     )
 
@@ -38,7 +58,10 @@ def post_create(request):
                 'Your community post has been created.',
             )
 
-            return redirect('community:post_list')
+            return redirect(
+                'community:post_detail',
+                pk=post.pk,
+            )
     else:
         form = CommunityPostForm()
 
@@ -47,5 +70,77 @@ def post_create(request):
         'community/post_form.html',
         {
             'form': form,
+            'page_title': 'Create a community post',
+            'button_text': 'Publish Post',
+        },
+    )
+
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(
+        CommunityPost,
+        pk=pk,
+        author=request.user,
+    )
+
+    if request.method == 'POST':
+        form = CommunityPostForm(
+            request.POST,
+            request.FILES,
+            instance=post,
+        )
+
+        if form.is_valid():
+            post = form.save()
+
+            messages.success(
+                request,
+                'Your community post has been updated.',
+            )
+
+            return redirect(
+                'community:post_detail',
+                pk=post.pk,
+            )
+    else:
+        form = CommunityPostForm(
+            instance=post,
+        )
+
+    return render(
+        request,
+        'community/post_form.html',
+        {
+            'form': form,
+            'page_title': 'Edit community post',
+            'button_text': 'Save Changes',
+        },
+    )
+
+
+@login_required
+def post_delete(request, pk):
+    post = get_object_or_404(
+        CommunityPost,
+        pk=pk,
+        author=request.user,
+    )
+
+    if request.method == 'POST':
+        post.delete()
+
+        messages.success(
+            request,
+            'Your community post has been deleted.',
+        )
+
+        return redirect('community:post_list')
+
+    return render(
+        request,
+        'community/post_confirm_delete.html',
+        {
+            'post': post,
         },
     )
