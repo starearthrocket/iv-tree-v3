a107e865d498fc72d5f627f4dec311a6969f4b01
starearthrocket <aliceharperoverton@yahoo.com>
2026-09-15 12:17:01 +0100

    Add custom production error pages

--- config/urls.py
+++ config/urls.py
@@ -13,8 +13,13 @@ urlpatterns = [
     path('support/', include('support.urls')),
 ]
 
+
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT,
     )
+
+
+handler404 = 'core.views.custom_404'
+handler500 = 'core.views.custom_500'
--- core/views.py
+++ core/views.py
@@ -2,4 +2,29 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'core/home.html')
+    """Display the I-V Tree homepage."""
+
+    return render(
+        request,
+        'core/home.html',
+    )
+
+
+def custom_404(request, exception):
+    """Display the custom page-not-found response."""
+
+    return render(
+        request,
+        'errors/404.html',
+        status=404,
+    )
+
+
+def custom_500(request):
+    """Display the custom server-error response."""
+
+    return render(
+        request,
+        'errors/500.html',
+        status=500,
+    )
